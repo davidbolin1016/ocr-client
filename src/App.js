1df495b8e6afa46d454dc13f24fc076daf8f8db1
David Bolin <david@bolin.at>
2020-05-11 15:02:32 -0700

    completed poc for client react app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      selectedImage: '',
+      content: ''
+    };
+
+  }
+
+  onChange = (ev) => {
+    this.setState({
+      selectedImage: ev.target.files[0]
+    });
+  }
+
+  onSubmit = (ev) => {
+    ev.preventDefault();
+    const data = new FormData();
+    data.append('image', this.state.selectedImage);
+    axios.post('http://localhost:8000/api/images/', data, {}).then(res => {
+      if (res.data && res.data.result) {
+        const finalResult = res.data.result.map(ele => ele.description).join('');
+        this.setState({
+          content: finalResult
+        });
+      } 
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          Slot Machine Reader
+        </header>
+        <div className="file-selection">
+          <form onSubmit={this.onSubmit}>
+              <h3>Choose File</h3>
+              <div className="form-group choice">
+                  <input type="file" capture="environment" accept="image/png, image/jpg, image/jpeg" onChange={this.onChange}/>
+              </div>
+              <div className="form-group">
+                  <button className="btn btn-primary" type="submit">Read Text</button>
+              </div>
+              <div className="text-box">
+                <h3>{this.state.content ? 'Text in Image:' : null}</h3>
+                {this.state.content}
+              </div>
+          </form>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
